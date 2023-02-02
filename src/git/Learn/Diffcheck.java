e32dd2c02e4ce8f5eb55a95cb40b4078d8a4d3a9
Aditi sharma <assharma.anjali12@gmail.com>
2023-02-02 20:46:39 +0530

    commit

--- src/git/Learn/Diffcheck.java
+++ src/git/Learn/Diffcheck.java
@@ -4,6 +4,9 @@ public class Diffcheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		System.out.println("the sum is " +(a+b));
 
 	}
 
